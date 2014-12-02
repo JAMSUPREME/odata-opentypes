68cca1e97d17e2e00ed5db8d5525e5cd9b2009f1
JAMSUPREME <jspencer.jms@gmail.com>
2014-12-02 14:41:04 -0500

    some cleanups so the hack necessary in order to add special serialization is more apparent in the client code

--- src/SampleODataClient2/Client/ExtraFieldsExtended.cs
+++ src/SampleODataClient2/Client/ExtraFieldsExtended.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SampleODataClient2.Client.SampleOdataHost
 {
-    //partial class ExtraFields
-    //{
-    //    [global::Microsoft.OData.Client.OriginalNameAttribute("key")]
-    //    public string Key { get; set; }
-    //}
-
     partial class ExtraFields
     {
         public int Worthless { get; set; }
